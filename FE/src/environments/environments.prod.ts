46f1a69faeef3232afa2f2919d9cff855717772e
Martin <martinkasabov98@gmail.com>
2026-06-14 22:12:24 +0300

    set prod apiUrl to the Railway BE

--- FE/src/environments/environments.prod.ts
+++ FE/src/environments/environments.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: "https://REPLACE_WITH_RAILWAY_BE_URL/app",
+    apiUrl: "https://vendo-production-8cad.up.railway.app/app",
     STRIPE_PUBLISHABLE_KEY: "pk_test_51ScNwmGY0NYtZ6EuG5Hats0KhuMdAF7gkuKN1moXI37HYa0WKqY2Y1yo0qvYXLdTzEhECRunmtC5f5MA9SlqgBWp00prkUYbpZ",
     X_CSCAPI_KEY: "Qjg3bGYzSjUzV2tUOGZDU1Z2SlFQQnhScXlJTWNVcnQ3Y093Tk53TA==",
 };
